4d5457483a7aabdd1160bd854610171b7ef3daa4
Dmitriy Basov <dmitrybasov_787788@mail.ru>
2024-10-16 00:59:09 +0300

    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задайте двумерный массив целых чисел. Напишите программу, которая
+// находит сумму элементов в каждом столбце массива и выводит её.
+// Пример входных данных:
+// 1 2 3
+// 4 5 6
+// 7 8 9
+// 10 11 12
+
+
+
+
+
+int[,] CreateMatrixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void SumColumsMatrix(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum = matrix[i, j] + sum;
+        }
+        Console.WriteLine($"сумма элементов в столбце {j}: {sum} ");
+    }
+
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 5, 1, 100);
+PrintMatrix(array2d);
+
+SumColumsMatrix(array2d);
+Console.WriteLine();
