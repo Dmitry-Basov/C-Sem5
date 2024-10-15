b84e56d728de23b5534ae2420ed172ae1bcd1287
Dmitriy Basov <dmitrybasov_787788@mail.ru>
2024-10-16 02:09:34 +0300

    Task7

---
+++ Task7/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задайте двумерный массив. Замените все отрицательные числа на их
+// абсолютные значения.
+// Пример входных данных:
+// 1 -2 3
+// -4 5 -6
+// 7 -8 9
+// Пример результата:
+// 1 2 3
+// 4 5 6
+// 7 8 9
+
+
+int[,] CreateMatrixRndInt(int rows, int cols, int min, int max)
+{
+    int[,] matrix = new int[rows, cols];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+void ReplaceNumbersMatrix(int[,] matrix)
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+     {
+         for (int j = 0; j < matrix.GetLength(1); j++)
+         {
+            if(matrix[i,j] < 0)
+            {
+                matrix[i,j] *= -1;
+
+            }
+         }
+     }
+Console.WriteLine("массив теперь выглядит так: ");
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(4,5,-10,10);
+PrintMatrix(array2d);
+
+ReplaceNumbersMatrix(array2d);
+Console.WriteLine();
+
